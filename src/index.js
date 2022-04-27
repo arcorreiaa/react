035693ff7c0f55b85d88a0e4e3594a5f5e198a32
Alysson Correia <arcorreiaa@gmail.com>
2022-04-26 21:25:38 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+function sum(a, b) {
+  return a + b;
+}
+
+function primeiroJSX() {
+  return (
+    <div className="teste">
+      Alysson Correia
+      <h1>Soma: {sum(10, 60)}</h1>
+    </div>
+  );
+}
+
+const App = () => {
+  return <div className="App">{primeiroJSX()}</div>;
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
