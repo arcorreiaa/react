62341ee5ad9be88d17b5d071ad2f1547644e2d6b
Alysson Correia <arcorreiaa@gmail.com>
2022-05-02 20:53:26 -0300

    introdução reactjs

--- src/index.js
+++ src/index.js
@@ -2,22 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-function sum(a, b) {
-  return a + b;
-}
+const App = () => {
+  const name = "digital innovation";
 
-function primeiroJSX() {
+  const handleChange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+  };
+
+  const showEvent = (e) => {
+    console.log("Evento clicado");
+    console.log(e);
+    alert(name);
+  };
+
+  const Button = <button onClick={showEvent}>Mostrar Eventos</button>;
+
   return (
-    <div className="teste">
-      Alysson Correia
-      <h1>Soma: {sum(10, 60)}</h1>
+    <div>
+      <p>Digital Innovation</p>
+      <p>Alysson Rychard</p>
+      <input onChange={handleChange} />
+      {Button}
     </div>
   );
-}
-
-const App = () => {
-  return <div className="App">{primeiroJSX()}</div>;
 };
+
+export default App;
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// function primeiroJSX() {
+//   return (
+//     <div className="teste">
+//       Alysson Correia
+//       <h1>Soma: {sum(10, 60)}</h1>
+//     </div>
+//   );
+// }
+
+// const App = () => {
+//   return <div className="App">{primeiroJSX()}</div>;
+// };
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
